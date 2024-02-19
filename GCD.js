ff48507aceaaaf92ec6ee25d6956201af73e65c3
= <1ahmed2adil3awan@gmail.com>
2024-02-20 01:59:04 +0500

    First two problems

---
+++ GCD.js
@@ -0,0 +1,42 @@
+// For two strings s and t, we say "t divides s" if and only if s = t + t + t + ... + t + t (i.e., t is concatenated with itself one or more times).
+
+// Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
+
+// Example 1:
+
+// Input: str1 = "ABCABC", str2 = "ABC"
+// Output: "ABC"
+// Example 2:
+
+// Input: str1 = "ABABAB", str2 = "ABAB"
+// Output: "AB"
+// Example 3:
+
+// Input: str1 = "LEET", str2 = "CODE"
+// Output: ""
+
+/**
+ * @param {string} str1
+ * @param {string} str2
+ * @return {string}
+ */
+var gcdOfStrings = function (str1, str2) {
+  if (str1 + str2 !== str2 + str1) {
+    return "";
+  }
+  const l1 = str1.length;
+  const l2 = str2.length;
+
+  let gcd = function (x, y) {
+    let R;
+    while (x % y > 0) {
+      R = x % y;
+      x = y;
+      y = R;
+    }
+    return y;
+  };
+
+  let div = gcd(l1, l2);
+  return str1.slice(0, div);
+};
