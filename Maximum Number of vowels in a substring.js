a63503d9827106d7d275f00f5e3574bf0feeed01
= <1ahmed2adil3awan@gmail.com>
2024-03-13 00:20:27 +0500

    Max number of vowels

--- Maximum Number of vowels in a substring.js
+++ Maximum Number of vowels in a substring.js
@@ -23,3 +23,25 @@
 // 1 <= s.length <= 105
 // s consists of lowercase English letters.
 // 1 <= k <= s.length
+
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var maxVowels = function (s, k) {
+  const vowels = ["a", "e", "i", "o", "u"];
+  let max = 0;
+  let count = 0;
+  let start = 0;
+  for (let end = 0; end < s.length; end++) {
+    if (vowels.includes(s[end])) count++;
+    if (end - start > k - 1) {
+      if (vowels.includes(s[start])) count--;
+      start++;
+    }
+    max = Math.max(max, count);
+    if (max === k) return max;
+  }
+  return max;
+};
