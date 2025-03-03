50c54871f01054e698f3979b697878f028671a02
adilawan1 <88656890+adilawan1@users.noreply.github.com>
2025-03-04 02:14:53 +0500

    [605] [Time Beats: 87.09%] [Memory Beats: 5.03%] - LeetPush

--- Can-Place-Flowers.js
+++ Can-Place-Flowers.js
@@ -4,14 +4,25 @@
  * @return {boolean}
  */
 var canPlaceFlowers = function(flowerbed, n) {
-            for (let i = 0; i < flowerbed.length; i++) {
-        const left = i === 0 || flowerbed[i - 1] === 0;
-        const right = i === flowerbed.length - 1 || flowerbed[i + 1] === 0;
-        
-        if (left && right && flowerbed[i] === 0) {
-            flowerbed[i] = 1;
-            n--;
+    if(n<=0) return true
+    for(let i=0; i<flowerbed.length ; i++){
+        if(i==0 && (flowerbed[i+1]==0||i+1>=flowerbed.length) && flowerbed[i]==0)
+        {
+            flowerbed[i]=1
+            n=n-1;
         }
+        else if(flowerbed[i+1]==0&&flowerbed[i]==0&&flowerbed[i-1]==0)
+        {
+            flowerbed[i]=1
+            n=n-1;
+        }
+        else if(i==flowerbed.length-1&&flowerbed[i]==0&&flowerbed[i-1]==0)
+        {
+            flowerbed[i]=1
+            n=n-1;
+        }
+
     }
-    return n <= 0;  
+
+    return n<=0
 };
