6e6d1358d2f0954e6dd605ddc110d9314d809954
adilawan1 <88656890+adilawan1@users.noreply.github.com>
2025-03-05 15:12:25 +0500

    [334] [Time Beats: 62.33%] [Memory Beats: 17.62%] - LeetPush

--- Increasing-Triplet-Subsequence.js
+++ Increasing-Triplet-Subsequence.js
@@ -3,14 +3,19 @@
  * @return {boolean}
  */
 var increasingTriplet = function(nums) {
+       if(nums.length >= 3){
     let lowNum=Infinity;
     let SecondNum=Infinity;
-
     for(let i of nums){
+                
+        if(i>lowNum && i>SecondNum)
+        return true
+        
         if(lowNum>i)
         lowNum=i
         else if(SecondNum>i && i>lowNum)
         SecondNum=i
     }
+       }
     return false
 };
