26582bcda24f12da0d48bc669a4b7c5fc7cf09f6
adilawan1 <88656890+adilawan1@users.noreply.github.com>
2025-03-09 16:18:40 +0500

    [392] [Time Beats: 100.00%] [Memory Beats: 23.89%] - LeetPush

--- Is-Subsequence.js
+++ Is-Subsequence.js
@@ -6,8 +6,10 @@
 var isSubsequence = function(s, t) {
     let firstPointer=0
     let secondPointer=0
+    const l1= s.length
+    const l2=t.length
 
-    while(firstPointer<s.length&&secondPointer<t.length)
+    while(firstPointer<l1&&secondPointer<l2)
     {
         if(s[firstPointer]==t[secondPointer])
         {
